7395cd72079f108724e4edb504f6cd2431aaaa7d
lucasweiby <lucas_weiby@hotmail.com>
2016-10-02 01:15:48 -0300

    Welcome screen has been created

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ACAQ • Bem-Vindo</title>
+	<link rel="stylesheet" type="text/css" href="css/welcome.css">
+	<?php include("index/views/head.php") ?>
+</head>
+<body>
+	<h1 class="first">Você tem alguma dúvida?</h1>
+	<h4 class="second">Deixe que nós encontremos a melhor resposta para você!</h4>
+	<form class="botoes pull-right" action="login.html" method="post">
+      <input class="botao1" type="submit" nome="login" value="SIGN IN"></input>
+      <button class="botao2">Ainda não tem cadastro?</button>
+    </form>
+</body>
+</html>
